4bddc3a7d7c639bd7b5d34d09992e194f8bec219
Jiaolong Yu <jiaolongyu@JiaolonsMacBook.attlocal.net>
2020-02-08 19:50:24 -0800

    fix merge

--- app/src/main/java/com/magic/elana/MainActivity.java
+++ app/src/main/java/com/magic/elana/MainActivity.java
@@ -1,7 +1,17 @@
 package com.magic.elana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ---------- BottomNavigation ----------------
+        BottomNavigationView bottomNavigation;
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                openFragment(new HomeFragment());
+                                return true;
+                            case R.id.navigation_sms:
+                                openFragment(new CreateFragment());
+                                return true;
+                            case R.id.navigation_notifications:
+                                openFragment(new SavedFragment());
+                                return true;
+                        }
+                        return false;
+                    }
+
+                };
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
